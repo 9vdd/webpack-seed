b9897c1815dacea84a5a827b9dad7bd779b46030
chendapeng <dapeng_chen@foxmail.com>
2017-08-04 13:31:08 +0800

    webpack config 文件

--- bin/server.js
+++ bin/server.js
@@ -1,1 +1,22 @@
-console.log('server')
+var config = require("./webpack.dev.config.js");
+var webpack = require('webpack');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+var WebpackDevServer = require('webpack-dev-server');
+var port = 9999;
+
+config.entry.index.unshift("webpack-dev-server/client?http://localhost:" + port + "/", 'webpack/hot/dev-server');
+config.plugins.push(new OpenBrowserPlugin({
+  url: 'http://localhost:' + port
+}));
+
+var compiler = webpack(config);
+var server = new WebpackDevServer(compiler, {
+  hot: true,
+  disableHostCheck: true,
+  stats: {
+    colors: true // 用颜色标识
+  },
+  contentBase: 'src'
+});
+server.listen(port);
