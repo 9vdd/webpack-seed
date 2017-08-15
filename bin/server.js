7e22517696f00ed9f9a3895b87a843578e0869df
chendapeng <dapeng_chen@foxmail.com>
2017-08-15 16:19:12 +0800

    add open browser plugin

--- bin/server.js
+++ bin/server.js
@@ -1,23 +1,25 @@
 var config = require("./webpack.dev.config.js");
 var webpack = require('webpack');
-// var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 var WebpackDevServer = require('webpack-dev-server');
 
+const port = 3000;
+
 // config.entry.index.unshift("webpack-dev-server/client?http://localhost:" + port + "/", 'webpack/hot/dev-server');
-// config.plugins.push(new OpenBrowserPlugin({
-//   url: 'http://localhost:' + port
-// }));
+config.plugins.push(new OpenBrowserPlugin({
+  url: 'http://localhost:' + port + '/index.html'
+}));
 
 var compiler = webpack(config);
 var server = new WebpackDevServer(compiler, {
   hot: true,
   disableHostCheck: true,
-  open: true,
-  openPage: '/index.html',
   stats: {
     colors: true // 用颜色标识
   },
   contentBase: 'src'
 });
-server.listen(3000);
+server.listen(port, '', () => {
+  console.log(new Date());
+});
