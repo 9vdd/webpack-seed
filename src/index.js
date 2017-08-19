358bdc7bc70810704381dac325d3593eb89fcf23
chendapeng <dapeng_chen@foxmail.com>
2017-08-19 13:39:24 +0800

    es2017 add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './style.css';
 
 console.log('hello world');
+
+let count = 0;
+
+function timer() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      count += 1;
+      console.log(count);
+      resolve(count);
+    });
+  });
+}
+
+document.getElementById('btn').addEventListener('click', async () => {
+  const num = await timer();
+  console.log('res', num);
+});
